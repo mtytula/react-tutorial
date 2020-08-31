4b12ebff93c53fafd76405d97bb42d159b461b20
mtytula <tytmarek@gmail.com>
2020-08-31 17:31:35 +0200

    Removed commented code

--- src/Weather/Weather.jsx
+++ src/Weather/Weather.jsx
@@ -11,39 +11,3 @@ export const WeatherComponent = ({ weatherData }) => (
 WeatherComponent.propTypes = {
     city: PropTypes.string
 }
-// export const WeatherComponent = ({ city }) ({
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         city: ""
-    //     }
-    // }
-
-    // componentDidMount() {
-    //     fetch("https://cors-anywhere.herokuapp.com/www.metaweather.com//api/location/search/?query=london")
-    //         .then(response => response.json())
-    //         .then((response) => {
-    //             fetch(`https://cors-anywhere.herokuapp.com/www.metaweather.com//api/location/${response[0].woeid}`)
-    //                 .then((response) => response.json())
-    //                 .then((response) => this.setState({weatherData: response}))
-    //         })
-    //
-    // }
-    //
-    // componentDidUpdate(prevProps, prevState, snapshot) {
-    // }
-    //
-    //
-    // handleOnClick = (city) => {
-    //     this.setState(this.state.city, city)
-    //     console.log(`You're looking for: ${this.state.city}`)
-    // }
-    //
-    // render() {
-    //     return (
-    //         <WeatherInput
-    //             handleOnClick={this.handleOnClick}
-    //         />
-    //     );
-    // }
-// })
